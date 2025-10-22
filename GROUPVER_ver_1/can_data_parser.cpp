0e86965be84a4356a60b795400aff6475af57222
Michi <michi.tan.xiu.ting@gmail.com>
2025-10-22 11:35:06 +0800

    Add files via upload

--- GROUPVER_ver_1/can_data_parser.cpp
+++ GROUPVER_ver_1/can_data_parser.cpp
@@ -26,6 +26,7 @@ void can_data_parse(twai_message_t &message) {
             if (message.data_length_code >= 2) {
                 vehicleData.ecu_byte0 = message.data[0];
                 vehicleData.ecu_byte1 = message.data[1];
+
                 vehicleData.ecu_valid = true;
                 vehicleData.last_update = millis();
                 
@@ -35,88 +36,88 @@ void can_data_parse(twai_message_t &message) {
             }
             break;
             
-        case CAN_ID_DATA_0x24:  // 0x24
-            for (int i = 0; i < message.data_length_code && i < 8; i++) {
-                vehicleData.data_0x24[i] = message.data[i];
+        case CAN_ID_BATTERY_STATS:  // 0x24
+            for (int i = 0; i < 8; i++) {
+                if (i < message.data_length_code) {
+                    vehicleData.data_0x24[i] = message.data[i];
+                } else {
+                    vehicleData.data_0x24[i] = 0;  // Fill remaining with zeros
+                }
             }
+
             vehicleData.data_0x24_valid = true;
             vehicleData.last_update = millis();
             
-            Serial.printf("[0x24] Data: ");
-            for (int i = 0; i < message.data_length_code; i++) {
-                Serial.printf("%02X ", message.data[i]);
-            }
-            Serial.println();
+            Serial.printf("[0x24] BATTERY Status - Byte0: 0x%02X (%d), Byte1: 0x%02X (%d), Byte2: 0x%02X (%d), Byte3: 0x%02X (%d), Byte4: 0x%02X (%d), Byte5: 0x%02X (%d), Byte6: 0x%02X (%d), Byte7: 0x%02X (%d)\n",
+                         vehicleData.data_0x24[0], vehicleData.data_0x24[0],
+                         vehicleData.data_0x24[1], vehicleData.data_0x24[1],
+                         vehicleData.data_0x24[2], vehicleData.data_0x24[2],
+                         vehicleData.data_0x24[3], vehicleData.data_0x24[3],
+                         vehicleData.data_0x24[4], vehicleData.data_0x24[4],
+                         vehicleData.data_0x24[5], vehicleData.data_0x24[5],
+                         vehicleData.data_0x24[6], vehicleData.data_0x24[6],
+                         vehicleData.data_0x24[7], vehicleData.data_0x24[7]);
             break;
             
-        case CAN_ID_DATA_0x34:  // 0x34
+        case CAN_ID_FL_Wheel_Speed:  // 0x34
             for (int i = 0; i < message.data_length_code && i < 8; i++) {
                 vehicleData.data_0x34[i] = message.data[i];
             }
+
             vehicleData.data_0x34_valid = true;
             vehicleData.last_update = millis();
             
-            Serial.printf("[0x34] Data: ");
-            for (int i = 0; i < message.data_length_code; i++) {
-                Serial.printf("%02X ", message.data[i]);
-            }
-            Serial.println();
+            Serial.printf("[0x34] FRONT LEFT WHEEL Speed - Byte0: 0x%02X (%d), Byte1: 0x%02X (%d)\n",
+                        vehicleData.data_0x34[0], vehicleData.data_0x34[0],
+                        vehicleData.data_0x34[1], vehicleData.data_0x34[1]);
             break;
             
-        case CAN_ID_DATA_0x35:  // 0x35
+        case CAN_ID_FR_Wheel_Speed:  // 0x35
             for (int i = 0; i < message.data_length_code && i < 8; i++) {
                 vehicleData.data_0x35[i] = message.data[i];
             }
             vehicleData.data_0x35_valid = true;
             vehicleData.last_update = millis();
             
-            Serial.printf("[0x35] Data: ");
-            for (int i = 0; i < message.data_length_code; i++) {
-                Serial.printf("%02X ", message.data[i]);
-            }
-            Serial.println();
+            Serial.printf("[0x35] FRONT RIGHT WHEEL Speed - Byte0: 0x%02X (%d), Byte1: 0x%02X (%d)\n",
+                        vehicleData.data_0x35[0], vehicleData.data_0x35[0],
+                        vehicleData.data_0x35[1], vehicleData.data_0x35[1]);
             break;
             
-        case CAN_ID_DATA_0x36:  // 0x36
+        case CAN_ID_RL_Wheel_Speed:  // 0x36
             for (int i = 0; i < message.data_length_code && i < 8; i++) {
                 vehicleData.data_0x36[i] = message.data[i];
             }
             vehicleData.data_0x36_valid = true;
             vehicleData.last_update = millis();
             
-            Serial.printf("[0x36] Data: ");
-            for (int i = 0; i < message.data_length_code; i++) {
-                Serial.printf("%02X ", message.data[i]);
-            }
-            Serial.println();
+            Serial.printf("[0x36] REAR LEFT WHEEL Speed - Byte0: 0x%02X (%d), Byte1: 0x%02X (%d)\n",
+                        vehicleData.data_0x36[0], vehicleData.data_0x36[0],
+                        vehicleData.data_0x36[1], vehicleData.data_0x36[1]);
             break;
             
-        case CAN_ID_DATA_0x37:  // 0x37
+        case CAN_ID_RR_Wheel_Speed:  // 0x37
             for (int i = 0; i < message.data_length_code && i < 8; i++) {
                 vehicleData.data_0x37[i] = message.data[i];
             }
             vehicleData.data_0x37_valid = true;
             vehicleData.last_update = millis();
             
-            Serial.printf("[0x37] Data: ");
-            for (int i = 0; i < message.data_length_code; i++) {
-                Serial.printf("%02X ", message.data[i]);
-            }
-            Serial.println();
+            Serial.printf("[0x37] REAR RIGHT WHEEL Speed - Byte0: 0x%02X (%d), Byte1: 0x%02X (%d)\n",
+                        vehicleData.data_0x37[0], vehicleData.data_0x37[0],
+                        vehicleData.data_0x37[1], vehicleData.data_0x37[1]);
             break;
             
-        case CAN_ID_DATA_0x38:  // 0x38
+        case CAN_ID_VEHICLE_Speed:  // 0x38
             for (int i = 0; i < message.data_length_code && i < 8; i++) {
                 vehicleData.data_0x38[i] = message.data[i];
             }
             vehicleData.data_0x38_valid = true;
             vehicleData.last_update = millis();
             
-            Serial.printf("[0x38] Data: ");
-            for (int i = 0; i < message.data_length_code; i++) {
-                Serial.printf("%02X ", message.data[i]);
-            }
-            Serial.println();
+            Serial.printf("[0x38] VEHICLE Overall Speed - Byte0: 0x%02X (%d), Byte1: 0x%02X (%d)\n",
+                        vehicleData.data_0x38[0], vehicleData.data_0x38[0],
+                        vehicleData.data_0x38[1], vehicleData.data_0x38[1]);
             break;
             
         default:
